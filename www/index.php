5d2eb81346fb540f0cbd85c4466c544f64defab9
NosovArtem <nosovartem1992@gmail.com>
2022-11-08 19:03:11 +0100

    строгая типизация

--- www/index.php
+++ www/index.php
@@ -1,37 +1,12 @@
 <?php
+declare(strict_types=1); // строгая типизация
 
-//$a = 6;
-//$b = 3;
-//
-//// Сложение
-//var_dump($a + $b);
-//
-//// Вычитание
-//var_dump($a - $b);
-//
-//// Умножение
-//var_dump($a * $b);
-//
-//// Деление
-//var_dump($a / $b);
-//
-//// Остаток от деления
-//var_dump($a % 4);
-//
-//// Возведение в степень
-//var_dump($a ** 2);
+function getSum(int $x, int $y)
+{
+    var_dump($x);
+    var_dump($y);
 
+    return $x + $y;
+}
 
-//var_dump(!1);
-//var_dump(1);
-//var_dump(!0);
-//var_dump(!true);
-//var_dump(2 && 3);
-//var_dump(5 && 0);
-//var_dump(3 || 0);
-//var_dump(5 / 1);
-//var_dump(1 / 5);
-//var_dump(5 + '5string');
-//var_dump('5' == 5);
-//var_dump('05' == 5);
-//var_dump('05' == '5');
+echo getSum(12.5, 10);
