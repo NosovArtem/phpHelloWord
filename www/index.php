e7927b3c1cb75e213955fa66b3d5ae6994b4f2cd
NosovArtem <nosovartem1992@gmail.com>
2022-11-08 17:58:08 +0100

    var_dump и операторы

--- www/index.php
+++ www/index.php
@@ -1,11 +1,37 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Пример встраивания PHP в HTML</title>
-</head>
-<body>
-<h1>Пример встраивания PHP в HTML</h1>
-2 + 2 = <?php echo 2 + 3; ?>
-</body>
-</html>
+<?php
+
+//$a = 6;
+//$b = 3;
+//
+//// Сложение
+//var_dump($a + $b);
+//
+//// Вычитание
+//var_dump($a - $b);
+//
+//// Умножение
+//var_dump($a * $b);
+//
+//// Деление
+//var_dump($a / $b);
+//
+//// Остаток от деления
+//var_dump($a % 4);
+//
+//// Возведение в степень
+//var_dump($a ** 2);
+
+
+//var_dump(!1);
+//var_dump(1);
+//var_dump(!0);
+//var_dump(!true);
+//var_dump(2 && 3);
+//var_dump(5 && 0);
+//var_dump(3 || 0);
+//var_dump(5 / 1);
+//var_dump(1 / 5);
+//var_dump(5 + '5string');
+//var_dump('5' == 5);
+//var_dump('05' == 5);
+//var_dump('05' == '5');
